1796b501467c24c13cd5d8624f42ae6e149d08c4
nanyangbai <bainanyang@outlook.com>
2021-11-09 18:15:37 +0800

    adapter demo

---
+++ src/main/java/com/example/adapterpattern/service/example_2/SquarePegAdapter1.java
@@ -0,0 +1,9 @@
+package com.example.adapterpattern.service.example_2;
+
+public class SquarePegAdapter1 {
+    public final RoundPeg roundPeg;
+
+    public SquarePegAdapter1(SquarePeg squarePeg) {
+        this.roundPeg = new RoundPeg(squarePeg.getRadius());
+    }
+}
